d437c2504a960c00f1a2f1b3a4d25265b75d9b03
JulianDbs <jdbsildesmode@gmail.com>
2022-05-19 00:02:38 -0300

    First Commit Extracted project from Zip generated with Spring Initializr.

---
+++ src/main/java/com/juliandbs/springmvcthymeleafexample/SpringmvcthymeleafexampleApplication.java
@@ -0,0 +1,13 @@
+package com.juliandbs.springmvcthymeleafexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcthymeleafexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcthymeleafexampleApplication.class, args);
+	}
+
+}
